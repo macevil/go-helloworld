c9ea363852dbf5ac7f3ad964995b30808a6229d1
Johannes Jungkunst <macevil@gmail.com>
2021-02-17 09:44:43 +0100

    fixed import

--- main.go
+++ main.go
@@ -1,29 +1,31 @@
 package main
 
 import (
-	"fmt"	"net/http"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
 )
-
 
 type Greeting struct {
 	Language string `json:"language"`
-	Content string `json:"content"`
+	Content  string `json:"content"`
 }
 
 func templateGreeting(s string) string {
-	if s == ""{
+	if s == "" {
 		s = "World"
 	}
 	return fmt.Sprint("Hello, %s!", s)
 }
 
-func handler(w http.ResponseWriter, r *http.Request){
+func handler(w http.ResponseWriter, r *http.Request) {
 	g := &Greeting{Language: "go", Content: templateGreeting(r.URL.Query().Get("name"))}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(g)
 }
 
-func main(){
+func main() {
 	http.HandleFunc("", handler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
